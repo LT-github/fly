af5c63292eea0a5d5df142c420ad98cf71efdc0f
DengJ <597529228@QQ.com>
2020-03-12 15:38:48 +0800

    添加赔率值不能为空

--- src/main/java/com/lt/fly/web/req/OddAdd.java
+++ src/main/java/com/lt/fly/web/req/OddAdd.java
@@ -3,11 +3,15 @@ package com.lt.fly.web.req;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class OddAdd {
 
+    @NotNull(message = "下注组不能为空")
     private Long betGroupId;
 
+    @NotNull(message = "赔率值不能为空")
     private Double oddValue;
 }
