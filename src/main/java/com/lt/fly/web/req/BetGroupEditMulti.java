06b559f325e50c094a6ccd7b753c6d1993f7256b
DengJ <597529228@QQ.com>
2020-03-15 09:49:20 +0800

    批量操作

--- src/main/java/com/lt/fly/web/req/BetGroupEditMulti.java
+++ src/main/java/com/lt/fly/web/req/BetGroupEditMulti.java
@@ -7,7 +7,13 @@ import java.util.List;
 
 @Getter
 @Setter
-public class BetGroupEditMulti extends BetGroupEdit {
+public class BetGroupEditMulti {
 
     private List<Long> ids;
+
+    private Integer singleBetting; //单次最低下注金额
+
+    private Integer maximumBet; 		 //最大下注金额
+
+    private Integer status; //玩法状态
 }
