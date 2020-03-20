d77618afe5a1fa35027bd17683ec27553316b2ad
DengJ <597529228@QQ.com>
2020-03-20 14:34:12 +0800

    handicap

--- src/main/java/com/lt/fly/web/controller/HandicapController.java
+++ src/main/java/com/lt/fly/web/controller/HandicapController.java
@@ -185,7 +185,7 @@ public class HandicapController extends BaseController {
         List<HandicapVo> handicapVos = HandicapVo.toVo(handicaps);
         HandicapVo handicapVo = new HandicapVo();
         handicapVo.setId(GlobalConstant.NoMemberHandicap.ID.getCode());
-        handicapVo.setName("无盘口");
+        handicapVo.setName("盘口Z");
         handicapVos.add(handicapVo);
         return HttpResult.success(handicapVos,"查询成功");
     }
