3cfdb115137f39d03d25cb5ac40e26f0991dec7f
DengJ <597529228@QQ.com>
2020-04-01 16:30:48 +0800

    会员报表

--- src/main/java/com/lt/fly/web/query/BetReportFind.java
+++ src/main/java/com/lt/fly/web/query/BetReportFind.java
@@ -1,12 +1,13 @@
 package com.lt.fly.web.query;
 
+import com.lt.fly.jpa.support.DataQueryObject;
 import com.lt.fly.utils.DateUtil;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class BetReportFind {
+public class BetReportFind implements DataQueryObject {
 
    private Long before = DateUtil.getDayStartTime(System.currentTimeMillis());
 
